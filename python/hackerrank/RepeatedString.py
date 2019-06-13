f03fcb2faaa1599cda75bf3b7c05984f798565c8
Amaury Quintero <amauryq@gmail.com>
2019-06-13 18:23:08 +0200

    Update RepeatedString.py

--- python/hackerrank/RepeatedString.py
+++ python/hackerrank/RepeatedString.py
@@ -8,6 +8,19 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
+    count_a = 0
+    for a in s:
+        if a == 'a':
+            count_a = count_a + 1
+    num, den = divmod(n, len(s))
+    num = num * count_a
+    new_s = a[0:den]
+    new_count_a = 0
+    for a in new_s:
+        if a == 'a':
+            new_count_a = new_count_a + 1
+
+    return num + new_count_a
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
