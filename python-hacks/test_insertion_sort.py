c5571555b892026b6c502f6afe357aebe87af217
Amaury Quintero <amauryq@gmail.com>
2020-04-22 22:14:45 +0000

    Add new file

---
+++ python-hacks/test_insertion_sort.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    # Generate an array of `ARRAY_LENGTH` items consisting
+    # of random integer values between 0 and 999
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    # Call the function using the name of the sorting algorithm
+    # and the array we just created
+    run_sorting_algorithm(algorithm="insertion_sort", array=array)
